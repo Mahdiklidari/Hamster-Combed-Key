385b61e2afa0880f2e0df5e1e7a9c2482699c0ee
MAHDI__MK__klidari <92400505+Mahdiklidari@users.noreply.github.com>
2024-09-19 14:46:04 +0330

    Update script.js
    Signed-off-by: MAHDI__MK__klidari <92400505+Mahdiklidari@users.noreply.github.com>

--- script.js
+++ script.js
@@ -42,134 +42,114 @@ document.addEventListener('DOMContentLoaded', () => {
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setTheme);
 
 document.addEventListener('DOMContentLoaded', () => {
-      const games = {
+       const games = {  
         1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            timing: 30000, // 30 seconds
-            attempts: 25,
-        },
-        2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
             timing: 30000, // 30 seconds
             attempts: 20,
-        },
-        3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 180000, // 180 seconds
-            attempts: 30,
-        },
-        4: {
+        },     
+        2: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
             timing: 30000, // 30 seconds
             attempts: 15,
         },
-        5: {
+        3: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
             timing: 30000, // 30 seconds
             attempts: 25,
         },
-        6: {
+        4: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             timing: 30000, // 30 seconds
             attempts: 20,
         },
-        7: {
+        5: {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        8: {
+        6: {
             name: 'Mow and Trim',
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        9: {
-            name: 'Mud Racing',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        10: {
+        
+        7: {
             name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        11: {
+        8: {
             name: 'Zoopolis',
             appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        12: {
+        9: {
             name: 'Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             timing: 20000, // 20 seconds
             attempts: 30,
         },
-        13: {
+        10: {
             name: 'Stone Age',
             appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
             promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
             timing: 20000, // 20 seconds
             attempts: 30,
         },
-       14: {
+       11: {
             name: 'Bouncemasters',
             appToken: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
             promoId: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
             timing: 20000, // 20 seconds
             attempts: 30,
         },
-        15: {
+        12: {
             name: 'Hide Ball',
             appToken: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600',
             promoId: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600',
             timing: 40000, // 30 seconds
             attempts: 30,
         },
-        16: {
+        13: {
             name: 'Pin Out Master',
             appToken: 'd2378baf-d617-417a-9d99-d685824335f0',
             promoId: 'd2378baf-d617-417a-9d99-d685824335f0',
             timing: 20000, // 30 seconds
             attempts: 30,
         },
-        17: {
+        14: {
             name: 'Count Masters',
             appToken: '4bdc17da-2601-449b-948e-f8c7bd376553',
             promoId: '4bdc17da-2601-449b-948e-f8c7bd376553',
             timing: 20000, // 30 seconds
             attempts: 30,
         },
-        18: {
+        15: {
             name: 'Infected Frontier',
             appToken: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
             promoId: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
             timing: 20000, // 30 seconds
             attempts: 30,
         },
-        19: {
+        16: {
             name: 'Among Water',
             appToken: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
             promoId: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
